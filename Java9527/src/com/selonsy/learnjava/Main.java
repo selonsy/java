3cd77c68ba8a3365f5d723c76447bc61f4ff2672
selonsy <selonsy@gmail.com>
2019-11-30 16:06:54 +0800

    update

---
+++ Java9527/src/com/selonsy/learnjava/Main.java
@@ -0,0 +1,18 @@
+package com.selonsy.learnjava;
+
+/**
+ * Copyright：Sichen International Co. Ltd.
+ *
+ * @author selonsy
+ * Created on 2019/11/28.
+ * Desc : none
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Java 9527");
+        int a=6;
+        int b=3;
+        int c=a+b;
+        System.out.println(c);
+    }
+}
---
+++ MyJava/src/Test.java
@@ -0,0 +1,16 @@
+/**
+ * Copyright：Sichen International Co. Ltd.
+ *
+ * @author selonsy
+ * Created on 2019/11/26.
+ * Desc : none
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Scratch 002");
+    }
+
+    public int add(int a,int b){
+        return a+b;
+    }
+}
